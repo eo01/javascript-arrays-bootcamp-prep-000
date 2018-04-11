4c8a81b6308fa6ad8e94463d1317ab91b80eec88
eo_01@protonmail.com <eo_01@protonmail.com>
2018-04-11 23:15:50 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -15,7 +15,7 @@ function destructivelyAddElementToEndOfArray(array, element){
   array.push(element);
   return array
 }
-function accessElementInArray (array, element)
+function accessElementInArray (array, index)
 return array[index];
 }
 function destructivelyRemoveElementFromBeginningOfArray (array){
