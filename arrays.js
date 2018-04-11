4b0918407841437ac176b8b5fb38a29ed0c66890
eo_01@protonmail.com <eo_01@protonmail.com>
2018-04-11 23:28:05 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -32,4 +32,6 @@ function destructivelyRemoveElementFromEndOfArray(array) {
 function removeElementFromEndOfArray(array) {
   return array.slice(0, array.length, -1)
 }
-
+function removeElementFromEndOfArray(array) {
+  return array.slice(0, array.length, -1)
+}
